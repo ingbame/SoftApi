8e8afb637be2c10d5a07bc43875723eba56b15ed
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-08-12 02:06:52 -0500

    Proyecto inicial

---
+++ KodiaksApi.Util/Extentions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodiaksApi.Util
+{
+    public static class Extensions
+    {
+        public static TCopy CopyProperties<TSource, TCopy>(this TSource source, TCopy copy)
+        {
+            if (source == null || copy == null)
+                throw new Exception("Both variables must be instantiated.");
+            PropertyInfo[] propSource = source.GetType().GetProperties();
+            foreach (PropertyInfo prop in propSource)
+            {
+                if (copy.GetType().GetProperties().Select(s => s.Name.ToLower()).Contains(prop.Name.ToLower()))
+                {
+                    var propToCopy = copy.GetType().GetProperties().Where(w => w.Name.ToLower() == prop.Name.ToLower()).FirstOrDefault();
+                    if (propToCopy.CanWrite)
+                        propToCopy.SetValue(copy, prop.GetValue(source, null), null);
+                }
+            }
+            return copy;
+        }
+    }
+}
---
+++ KodiaksApi/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WebApi to Kodiaks Softball Team",
+        Version = "v1",
+        Description = "Esta Api es la documentación para el uso de la App",
+        Contact = new OpenApiContact
+        {
+            Name = "ISC. Baruch Medina",
+            Email = "ingbame@gmail.com",
+            Url = new System.Uri("https://github.com/ingbame")
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Kodiaks");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
