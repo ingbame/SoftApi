0a6d14068a9288ba6d4c645d87906741e78ff31a
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-08-21 16:47:12 -0500

    Baruch: Configuracion de la capa Data, antes de ejecutar el Scaffold-DbContext

---
+++ KodiaksApi.Data/Context/DbContextConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodiaksApi.Data.Context
+{
+    public class DbContextConfig
+    {
+        public KodiaksDbContext CreateDbContext()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<KodiaksDbContext>();
+            var connectionString = configuration.GetConnectionString("MuayThaiConn");
+            builder.UseSqlServer(connectionString);
+            return new KodiaksDbContext(builder.Options);
+        }
+    }
+}
---
+++ KodiaksApi.Data/Context/KodiaksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodiaksApi.Data.Context
+{
+    public class KodiaksDbContext : DbContext
+    {
+        public KodiaksDbContext(DbContextOptions<KodiaksDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
