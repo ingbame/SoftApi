7d127eb79234a97ecaf85523851896e649143388
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-08-23 00:54:43 -0500

    Prueba de sincronización con seguridad

--- KodiaksApi/Controllers/SecurityController.cs
+++ KodiaksApi/Controllers/SecurityController.cs
@@ -1,4 +1,5 @@
 ﻿using KodiaksApi.Core;
+using KodiaksApi.Entity.Application;
 using KodiaksApi.Entity.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -63,7 +64,7 @@ namespace KodiaksApi.Controllers
         }
         [HttpPost("CreateNewPerson")]
         [Authorize(Roles = "SuperAdmin")]
-        public ActionResult CreateNewPerson(CredentialsEntity credential)
+        public ActionResult CreateNewMember(CredentialsEntity credential)
         {
             if (credential == null)
                 return NoContent();
@@ -93,6 +94,40 @@ namespace KodiaksApi.Controllers
             if (personaResult.Error)
                 return BadRequest(personaResult.Message);
             return Ok(personaResult.Model);
+        }
+        [HttpPost("EditMember")]
+        [Authorize]
+        public ActionResult EditMember(MemberEntity credential)
+        {
+            //if (credential == null)
+            //    return NoContent();
+
+            //if (string.IsNullOrEmpty(credential.User.UserName?.Trim()))
+            //    return BadRequest("Nombre de usuario vacío.");
+
+            //if (string.IsNullOrEmpty(credential.User.Password?.Trim()))
+            //    return BadRequest("Contraseña vacía.");
+
+            //if (string.IsNullOrEmpty(credential.Member.FullName?.Trim()))
+            //    return BadRequest("Nombre de persona vacío.");
+
+            //if (string.IsNullOrEmpty(credential.Member.NickName?.Trim()))
+            //    credential.Member.NickName = credential.User.UserName;
+
+            //if (credential.Member.Birthday == null)
+            //    return BadRequest("Revise su fecha de nacimiento.");
+
+            //if (credential.Member.Birthday < DateTime.Parse("1940-01-01"))
+            //    return BadRequest("Excede los 80 años, revise su fecha de nacimiento.");
+
+            //if (string.IsNullOrEmpty(credential.Member.CellPhoneNumber?.Trim()))
+            //    return BadRequest("Número de celular vacío.");
+
+            //var personaResult = BoSecurity.Instance.CreateNewPerson(credential);
+            //if (personaResult.Error)
+            //    return BadRequest(personaResult.Message);
+            //return Ok(personaResult.Model);
+            return NotFound();
         }
     }
 }
