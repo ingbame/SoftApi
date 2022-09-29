ae76873e7594cade1c983d190b3154198b2901d1
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-09-28 21:49:08 -0500

    Pubicación a prod

--- KodiaksApi/Program.cs
+++ KodiaksApi/Program.cs
@@ -61,7 +61,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
